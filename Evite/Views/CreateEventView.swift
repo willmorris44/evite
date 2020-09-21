cbd33018a506ca02fc3d7ceade5ec93148b53da9
Will Morris <william.morris117@gmail.com>
2020-09-21 12:17:14 -0400

    Adds createView layout

--- Evite/Views/CreateEventView.swift
+++ Evite/Views/CreateEventView.swift
@@ -9,8 +9,41 @@
 import SwiftUI
 
 struct CreateEventView: View {
+    @State private var title: String = ""
+    @State private var date: Date = Date()
+    @State private var place: String = ""
+    
+    init() {
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().barTintColor = .primary
+    }
+    
     var body: some View {
-        Text("Create Event View")
+        NavigationView {
+            VStack {
+                VStack {
+                    TextField("Title", text: $title)
+                        .font(.title)
+                        .padding(.top)
+                    
+                    TextField("Where", text: $place)
+                        .font(.title)
+
+                    if #available(iOS 14.0, *) {
+                        DatePicker("", selection: $date)
+                            .datePickerStyle(GraphicalDatePickerStyle())
+                            .frame(width: 300)
+                    } else {
+                        DatePicker("", selection: $date)
+                    }
+                }
+                .padding()
+                
+                BigCardView()
+            }
+            .navigationBarTitle("Create", displayMode: .inline)
+            .padding(.vertical)
+        }
     }
 }
 
